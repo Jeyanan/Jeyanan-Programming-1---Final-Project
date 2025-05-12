f391cf0d19c31486b6c73c608f73c97cb6cce7f2
Jeyanan <jeyanan07@gmail.com>
2025-05-12 01:42:10 -0400

    Added a new method

--- ActivityTracker.java
+++ ActivityTracker.java
@@ -6,32 +6,54 @@ import java.util.ArrayList;
  * @author (your name)
  * @version (a version number or a date)
  */
-public class ActivityTracker {
+public class ActivityTracker 
+{
     private ArrayList<Athlete> athletes;
     private ArrayList<Activity> activities;
     private ArrayList<Equipment> equipmentList;
 
-    public ActivityTracker() {
+    public ActivityTracker() 
+    {
         athletes = new ArrayList<>();
         activities = new ArrayList<>();
         equipmentList = new ArrayList<>();
     }
 
-    public void addAthlete(Athlete athlete) {
-        if (athlete != null) {
+    public void addAthlete(Athlete athlete)
+    {
+        if (athlete != null) 
+        {
             athletes.add(athlete);
         }
     }
 
-    public void addActivity(Activity activity) {
-        if (activity != null) {
+    public void addActivity(Activity activity)
+    {
+        if (activity != null) 
+        {
             activities.add(activity);
         }
     }
 
-    public void addEquipment(Equipment equipment) {
-        if (equipment != null) {
+    public void addEquipment(Equipment equipment)
+    {
+        if (equipment != null)
+        {
             equipmentList.add(equipment);
         }
+    }
+    
+    public void listAllAthletes() 
+    {
+        if (athletes.isEmpty()) 
+        {
+            System.out.println("No athletes registered.");
+        } else  {
+            System.out.println("List of Athletes:");
+            for (Athlete athlete : athletes)
+            {
+                System.out.println(athlete);  // assumes toString() is implemented
+            }
+        }    
     }
 }
